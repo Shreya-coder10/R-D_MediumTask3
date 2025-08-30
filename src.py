6c8b179065b49d4016fdf9d3828958fbb87017c9
Shreya-coder10 <shreyakumari4233@gmail.com>
2025-08-30 18:45:32 +0530

    Create src.py

---
+++ src.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+def read_maze(filename):
+    with open(filename, 'r') as file:
+        maze = [list(line.strip()) for line in file if line.strip()]
+    return maze
+
+def find_position(maze, char):
+    for i, row in enumerate(maze):
+        for j, cell in enumerate(row):
+            if cell == char:
+                return (i, j)
+    return None
+
+def print_maze(maze, path=None):
+    if path:
+        for i, j in path:
+            if maze[i][j] not in ['S', 'E']:
+                maze[i][j] = '.'
+    for row in maze:
+        print(''.join(row))
+    print()
+
+def solve_maze(start, end, maze):
+    """
+    BFS to find the shortest path from start to end in the maze.
+    Maze uses:
+      'S' = Start
+      'E' = End
+      '#' = Wall
+      ' ' = Open path
+    Returns a list of (row, col) tuples for the path, or None if no path exists.
+    """
+    rows, cols = len(maze), len(maze[0])
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # Down, Up, Right, Left
+
+    queue = deque([(start, [start])])  # store (position, path_so_far)
+    visited = set([start])
+
+    while queue:
+        (r, c), path = queue.popleft()
+
+        # If reached the end, return the path
+        if (r, c) == end:
+            return path
+
+        # Explore neighbors
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols:
+                if maze[nr][nc] != '#' and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    queue.append(((nr, nc), path + [(nr, nc)]))
+
+    return None  #If No path found
+
+
+if __name__ == '__main__':
+    filename = r'src\maze.txt'  #Make sure to set the path correctly
+    maze = read_maze(filename)
+    start = find_position(maze, 'S')
+    end = find_position(maze, 'E')
+
+    print("Original Maze:")
+    print_maze(maze)
+
+    path = solve_maze(start, end, maze)
+
+    if path:
+        print("Solved Maze with path:")
+        print_maze(maze, path)
+    else:
+        print("No path found.")
